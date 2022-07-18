2189f16c41d57e474292273dfa97492d6097d335
wjwow007 <yiwoojungDev@gmail.com>
2022-07-18 23:13:14 +0900

    Edit Backend-class

--- class_backend/Board.postgres.ts
+++ class_backend/Board.postgres.ts
@@ -1,23 +1,20 @@
 // 데이터베이스와 백엔드 연결해주기
-
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
- export class Board extends BaseEntity{
-	@PrimaryGeneratedColumn("increment")
-	//키 값을 자동으로 만들어 줌
-	number!: number;
+export class Board extends BaseEntity {
+  // 키 값을 자동으로 만들어 줌
+  @PrimaryGeneratedColumn("increment") // uuid
+  number!: number;
 
+  @Column({ type: "text" })
+  writer!: string;
 
-	@Column({type: "text"})
-	writer!: string;
+  @Column({ type: "text" })
+  title!: string;
 
-	@Column({type: "text"})
-	title!: string;
+  @Column({ type: "text" })
+  contents!: string;
 
-	@Column({type: "text"})
-	contents!: string;
-
-	//deletedAt : Date //soft-delete
-
+  //deletedAt : Date //soft-delete
 }
--- class_backend/bbb.ts
+++ class_backend/bbb.ts
@@ -1,14 +1,9 @@
 //데코레이터는 뭘까요?
-
-function qqq(aaa:any){
-
-    console.log("=================")
-    console.log(aaa)
-    console.log("================")
+function qqq(aaa: any) {
+  console.log("=================");
+  console.log(aaa);
+  console.log("================");
 }
-
 
 @qqq
-class Product{
-
-}
+class Product {}
